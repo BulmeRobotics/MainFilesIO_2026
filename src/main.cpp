63eecc64ccd62a2dbf46e8df36255258b33f71eb
VinRoh07 <vincent.rohkamm@web.de>
2025-11-18 14:15:41 +0100

    Main program Architecture

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,71 @@
+// author: Vincent Rohkamm
+// date: 18.11.2025
+// description: Main Program file for Robot
+
+#ifdef VISUAL_STUDIO
+  #pragma region Defines //--------------------------------------------
+#endif
+
+  #define BAUD_RATE 115200
+  #define I2C_CLOCK 1000000UL
+
+
+
+#ifdef VISUAL_STUDIO
+  #pragma endregion Defines
+  #pragma region Includes //-------------------------------------------
+#endif
+
+//INcludes
 #include <Arduino.h>
+#include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#ifdef VISUAL_STUDIO
+  #pragma endregion Includes
+  #pragma region Objects //--------------------------------------
+#endif
+
+//Objects
+
+
+#ifdef VISUAL_STUDIO
+  #pragma endregion Objects
+  #pragma region Variables //------------------------------------
+#endif
+
+//Variables
+
+#ifdef VISUAL_STUDIO
+  #pragma endregion Variables
+  #pragma region Prototypes //------------------------------------
+#endif
+
+  //FUNKTIONEN
+
+
+#ifdef VISUAL_STUDIO
+  #pragma endregion Functions
+  #pragma region Initialization //--------------------------------
+#endif
+
+int main(void) {
+  Serial.begin(BAUD_RATE);
+  Wire.begin(I2C_CLOCK);
+  Wire1.begin();
+  
+
+
+#ifdef VISUAL_STUDIO
+  #pragma endregion Initialization
+  #pragma region Cyclic //----------------------------------------
+#endif
+
+
+
 }
+#ifdef VISUAL_STUDIO
+  #pragma endregion Cyclic
+  #pragma region Functions //------------------------------------
+#endif
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
