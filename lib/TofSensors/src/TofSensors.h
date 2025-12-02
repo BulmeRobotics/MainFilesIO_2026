53d9f59a54067e58f2bd4e56672be6caddb51e8d
Florian Wiesner <florian.wiesner@bulme.at>
2025-12-02 14:18:24 +0100

    first draw of TofSensors

---
+++ lib/TofSensors/src/TofSensors.h
@@ -0,0 +1,23 @@
+#pragma once
+
+// Name:    TofSensors.h
+// Created:	18.11.2025
+// Author:  Florian Wiesner
+// Description: Header file for Time-of-Flight Sensors  
+
+// Libraries
+#include <Arduino.h>
+#include <VL6180X.h>
+
+class SingleVL180X;
+
+class TofSensors {
+    private:
+
+    public:
+    // Constructor
+    TofSensors();
+
+    // Objects
+
+};
