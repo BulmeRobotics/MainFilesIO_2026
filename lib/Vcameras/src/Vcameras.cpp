eb55d70d95baccff69400d6220bcfa90482e96dd
VinRoh07 <vincent.rohkamm@web.de>
2026-04-01 22:20:55 +0200

    added Camera: -Recieve -Enable

--- lib/Vcameras/src/Vcameras.cpp
+++ lib/Vcameras/src/Vcameras.cpp
@@ -1,10 +1,99 @@
 #include "Vcameras.h"
+#include <UserInterface.h>
 
-Vcameras::Vcameras(/* args */)
-{
+//---------------------------------------------------------------------------------------------------------
+// Initialization
+//---------------------------------------------------------------------------------------------------------
+
+ErrorCodes Vcameras::Init(Ejector* ejector, Mapping* mapper, Driving* robot, UserInterface* ui){
+    _ejector = ejector;
+    _mapper = mapper;
+    _robot = robot;
+    _ui = ui;
+
+    _camL->print("<I>");
+    if(Recieve(_camL) == ErrorCodes::OK){
+        if(_response.indexOf("OK") == -1) return ErrorCodes::ERROR;
+    }
+
+    _camR->print("<I>");
+    if(Recieve(_camR) == ErrorCodes::OK){
+        if(_response.indexOf("OK") == -1) return ErrorCodes::ERROR;
+    }
+    return ErrorCodes::OK;  
 }
 
-Vcameras::~Vcameras()
-{
+//---------------------------------------------------------------------------------------------------------
+// Recieve UART
+//---------------------------------------------------------------------------------------------------------
+
+ErrorCodes Vcameras::Recieve(Stream* ifc, uint16_t timeout){
+    uint32_t start = millis();
+    String buffer;
+
+    // Read until timeout or until we have at least one complete token <...>
+    while (millis() - start <= timeout) {
+        while (ifc->available()) {
+            int c = ifc->read();
+            if (c < 0) break;
+            buffer += (char)c;
+        }
+
+        int s = buffer.indexOf('<');
+        int e = (s >= 0) ? buffer.indexOf('>', s) : -1;
+        if (s >= 0 && e > s) break; // have a complete token
+    }
+
+    if (buffer.isEmpty()) {
+        _ui->ShowPopup("Camera timeout!", ErrorCodes::ERROR);
+        return ErrorCodes::TIMEOUT;
+    }
+
+    int s = buffer.indexOf('<');
+    int e = (s >= 0) ? buffer.indexOf('>', s) : -1;
+    if (s >= 0 && e > s) {
+        _response = buffer.substring(s + 1, e);
+        return ErrorCodes::OK;
+    }
+    _ui->ShowPopup("response invalid!", ErrorCodes::warning);
+    return ErrorCodes::invalid;
 }
 
+//---------------------------------------------------------------------------------------------------------
+// Enable
+//---------------------------------------------------------------------------------------------------------
+
+ErrorCodes Vcameras::Enable(bool en, ErrorCodes side){
+    Stream* ifc = (side == ErrorCodes::left) ? _camL : _camR;
+
+    if(en){ //Enable
+        ifc->print("<E>");
+        if(Recieve(ifc) == ErrorCodes::OK){
+            if(_response.indexOf("OK") == -1) return ErrorCodes::ERROR;
+
+            if(side == ErrorCodes::left) _LeftEnabled = true;
+            else _RightEnabled = true;
+            return ErrorCodes::OK;
+        }
+    } else {    //Disable
+        ifc->print("<D>");
+        if(Recieve(ifc) == ErrorCodes::OK){
+            if(_response.indexOf("OK") == -1) return ErrorCodes::ERROR;
+
+            if(side == ErrorCodes::left) _LeftEnabled = false;
+            else _RightEnabled = false; 
+            return ErrorCodes::OK;
+        }
+    }
+
+    _ui->ShowPopup("cams enable error", ErrorCodes::ERROR);
+    return ErrorCodes::invalid;
+}
+
+//---------------------------------------------------------------------------------------------------------
+// Enable
+//---------------------------------------------------------------------------------------------------------
+
+ErrorCodes Vcameras::Handler(){
+
+}
--- lib/Vcameras/src/Vcameras.h
+++ lib/Vcameras/src/Vcameras.h
@@ -8,17 +8,70 @@
 
 #include <CustomDatatypes.h>
 #include <Ejector.h>
+#include <Driving.h>
+#include <Mapping.h>
+
+class UserInterface;
 
 class Vcameras
 {
 private:
-    /* data */
+    // --- Hardware Info ---
+    static constexpr uint8_t CAMERA1_PIN_INT = 40;
+    static constexpr uint8_t CAMERA1_PIN_RST = 42;
+    static constexpr uint8_t CAMERA2_PIN_INT = 41;
+    static constexpr uint8_t CAMERA2_PIN_RST = 43;
 
+    // --- related Objects ---
+    Ejector* _ejector = nullptr;
+    Driving* _robot = nullptr;
+    Mapping* _mapper = nullptr;
+    UserInterface* _ui = nullptr;
+
+    // --- Interface ---
+    Stream* _camL = nullptr;
+    Stream* _camR = nullptr;
+
+    // --- State Fields ---
     bool _LeftEnabled, _RightEnabled;
     bool _LeftAlert, _RightAlert;
+
+    // --- Response ---
+    String _response = "";
+
+    // --- helper Methods
+    ErrorCodes Recieve(Stream* ifc, uint16_t timeout = 200);
 public:
-    Vcameras(/* args */);
-    ~Vcameras();
+    /**
+     * @brief Constructor for VictimCamera class
+     * @param camL ifc for left Cam
+     * @param camR ifc for right Cam
+     */
+    Vcameras(Stream* camL = &Serial2, Stream* camR = &Serial3) : _camL(camL), _camR(camR) {}
+
+    /**
+     * @brief Initializes Cam class. Tries connecting to cameras
+     * @return OK - success, else ErrorCodes
+     */
+    ErrorCodes Init(Ejector* ejector, Mapping* mapper, Driving* robot, UserInterface* ui);
+
+    /**
+     * @brief enables or disables Camera
+     * @param en true...on, false...off
+     * @param side left / right
+     * @return OK / Error
+     */
+    ErrorCodes Enable(bool en, ErrorCodes side);
+
+    /**
+     * @brief camera handler has to be called periodically
+     * @return ErrorCodes for debugging
+     */
+    ErrorCodes Handler();
+
+
+
+
 
 
     /**
