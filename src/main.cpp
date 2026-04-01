25c978c70c8ca4d13208e0071c83b8c8ec6feff4
Florian Wiesner <florian.wiesner@bulme.at>
2026-04-01 10:58:57 +0200

    tested mapping update, implemented gyro reset, improved startAlign(), bumper Error

--- src/main.cpp
+++ src/main.cpp
@@ -157,6 +157,11 @@ while (true) {
   if (currentMenuState == RobotState::RUN) {
     if (currentRunState == RunState::INITIAL) { //Initial Run Logic
       mapper.Reset();
+      robot.enableBumpers();	//Enable Bumpers
+			robot.startAlign();	//Start Aligning	
+			gyro.ResetAllAngles();	//Gyro angle zero
+			robot.maxRampIncline = 0;
+			robot.currentRobotHeight = 0;
       currentRunState = RunState::SETTILE;
     }
 
@@ -350,6 +355,11 @@ void cyclicMainTask() {
 }
 void cyclicRunTask() {
   tof.Update();
+
+  //Bumper Handling
+	if(currentRunState != RunState::INITIAL){
+		if(robot.bumperHandler() == ErrorCodes::BUMPER_WALL) currentRunState = RunState::SETTILE;
+	}
 }
 
 void ISR_BTN_BLACK() {
